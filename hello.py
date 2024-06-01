11bed6f5c08631c1c2b5dcb8fd3feded2100c00a
DharPartha <DharPartha1602@outlook.com>
2024-06-01 13:51:16 -0400

    Add files via upload
    Basic Hello World program in Python, using a Spyder UI

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 13:30:54 2024
+
+@author: DharP
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
